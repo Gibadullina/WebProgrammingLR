40c00a82446041bee98f351acf1f59f28a118a33
Gibadullina <gibadullina.elina@inbox.ru>
2021-11-09 01:52:35 +0500

    Change lab-1-3

--- lab-1-3.php
+++ lab-1-3.php
@@ -1,6 +1,21 @@
 <?php
-const NUM_E = 2.71828;
-echo 'Число е равно '.NUM_E.'<br>';
-$num_e1 = NUM_E;
-echo '$num_e1 = '.$num_e1. ' - '.gettype($num_e1).'<br>';
+   const NUM_E = 2.71828;
+   echo 'Число е равно '.NUM_E.'<br>';
+   $num_e1 = NUM_E;
+   echo '$num_e1 = '.$num_e1. ' - '.gettype($num_e1).'<br>';
+     echo '<br>';
+//1
+     echo 'Строковый тип'.'<br>';
+     settype($num_e1, string);
+     echo '$num_e1 = '.$num_e1. ' - '.gettype($num_e1).'<br>';
+     echo '<br>';
+//2
+     echo 'Целый тип'.'<br>';
+     settype($num_e1, integer);
+     echo '$num_e1 = '.$num_e1. ' - '.gettype($num_e1).'<br>';
+     echo '<br>';
+//3
+     echo 'Булевый тип'.'<br>';
+     settype($num_e1, bool);
+     echo '$num_e1 = '.$num_e1. ' - '.gettype($num_e1).'<br>';
 ?>
