0d7984dc8971f5045f3b6b6723cf7bcd00b54098
Gibadullina <gibadullina.elina@inbox.ru>
2021-11-09 02:01:53 +0500

    Change index.php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
  $d=date("d.m.Y H:i");
  echo($d);
 ?> 
-<p>Лабораторная № 1<p> 
+<p>Лабораторная работа № 1<p> 
     <ul>
  <li><a href="http://phpgibadullina/lab-1-1.php">Lab-1-1</a></li>
  <li><a href="http://phpgibadullina/lab-1-2.php">Lab-1-2</a></li>
  <li><a href="http://phpgibadullina/lab-1-3.php">Lab-1-3</a></li>
+ <li><a href="http://phpgibadullina/lab-1-5.php">Lab-1-5</a></li>
     </ul>
