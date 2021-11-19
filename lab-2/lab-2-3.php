77edce61f739c756137190cce6e409ec7a316e8f
Gibadullina <gibadullina.elina@inbox.ru>
2021-11-19 23:23:59 +0500

    Finished lab 2-3

--- lab-2/lab-2-3.php
+++ lab-2/lab-2-3.php
@@ -7,8 +7,35 @@
                "city" => "London",
 			   "snum" => "1001");
 			   
-	foreach($myarr as $key => $value)
-          {
-     echo "$key = $value <br />";
+	 foreach($myarr as $key => $value){
+      echo "$key = $value <br />";
           }
+		  
+  //Добавление элемента
+    echo '<br>Добавление элемента:<br>';
+	$myarr["rating"]=100;
+		foreach($myarr as $key => $value){
+             echo "$key = $value <br />";
+          }
+		  
+	//Сортировка элементов массива
+	   echo '<br>Сортировка элементов массива:<br>';
+	   asort ($myarr);
+	  		foreach($myarr as $key => $value){
+             echo "$key = $value <br />";
+          } 
+
+	
+	//Сортировка ключей массива
+	   echo '<br>Сортировка ключей массива:<br>';	
+	   	   ksort ($myarr);
+		foreach($myarr as $key => $value){
+          echo "$key = $value <br />";
+          }	
+	//Сортировка with sort()
+		   echo '<br>Сортировка массива:<br>';
+	   sort ($myarr);
+	  		foreach($myarr as $key => $value){
+             echo "$key = $value <br />";
+          } 
 ?>
