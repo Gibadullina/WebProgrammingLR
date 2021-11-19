7f910dde2c5ef12d0ee6cc4d81314ce495191b93
Gibadullina <gibadullina.elina@inbox.ru>
2021-11-19 22:40:03 +0500

    start lab 2-3

--- lab-2/lab-2-3.php
+++ lab-2/lab-2-3.php
@@ -1,3 +1,14 @@
 <?php
-
+  
+  //Ассоциативный массив
+  echo 'Ассоциативный массив:'.'<br>';
+    $myarr = array("cnum" => "2001",
+               "cname" => "Hoffman",
+               "city" => "London",
+			   "snum" => "1001");
+			   
+	foreach($myarr as $key => $value)
+          {
+     echo "$key = $value <br />";
+          }
 ?>
