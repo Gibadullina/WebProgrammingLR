11ace1ccada4bea198d482b49172c2d292cb1c54
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-10 23:38:18 +0500

    Task 3.6

--- lab-3/task/s3-6.php
+++ lab-3/task/s3-6.php
@@ -1,22 +1,55 @@
 <? if ($_POST["f"].checked==checked)
  $text=$_POST["t"];
- $f1=$_POST["f1"];
- $f2=$_POST["f2"];
- $f3=$_POST["f3"];
- $a=$_POST["a"];
- $b=$_POST["b"];
+
  $c=$_POST["c"];
- echo "Helllp <br>";
- if ((is_null($text))){
+ if (!(strlen($text))){
 	 echo "Вы не ввели корректный текст";
  } else {
-	 echo mb_strlen($text)."<br>";
-	 $tok = strtok($text, " ");
+	 echo "Ваш текст: ".$text."<br>";
+	if (isset($_POST["f1"]) and isset($_POST["a"])) {
+	 $a=$_POST["a"];
+	 if ($a<10) {
+	 switch ($a){
+		 case 1:
+		 $t1=$a." буквы:<br>";
+		 break;
+		 default:
+		 $t1=$a." букв: <br>";
+		 break;		 
+	 }
+ echo "Список слов из Вашего текста, которые состоят из ".$t1;
+ 	 $tok = strtok($text, " ");
   while ($tok !== false) {
-    echo mb_strlen($tok)."<br>";
+	 if ((mb_strlen($tok))==$a)
+    echo $tok."<br>";
     $tok = strtok(" ");
-}
+  } 
+  } else {echo "Количество букв не должно превышать 10";
+  }
+ }  if (isset($_POST["f2"])){
+	   $str2=mb_str_split ($text);
+	   echo "Преобразованный текст: ";
+	  for ($i=0; $i<=(count($str2));$i++) {
+		  if( preg_match("/[A-Za-z]/", $str2[$i]) ) {
+			
+			  $str2[$i]=mb_strtoupper($str2[$i]);}
+	  echo $str2[$i];
+	  
+	  }
+    }
+    if (isset($_POST["f3"]) and isset($_POST["b"]) and isset($_POST["c"])) {
+		if ((mb_strlen($_POST["b"])==1) and (mb_strlen($_POST["c"])==1)) {
+	  $b=$_POST["b"];
+	  echo "<br>Число вхождений символа ".$b." в строку: ".(substr_count($text,$b));
+	  $b=$_POST["c"];
+	  echo "<br>Число вхождений символа ".$c." в строку: ".(substr_count($text,$c));
+	}	}
  }
  
 echo ("<BR> <A href='s3-6.html'> Вернуться назад </A>");
+function mb_str_split($str)
+    {
+        preg_match_all('#.{1}#uis', $str, $out);
+        return $out[0];
+    }
 ?>
