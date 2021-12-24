32b60e3241379e7b70cc02ce46635e59eb770cff
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 02:42:01 +0500

    Save_new.php edit

--- bd_games/games/save_new.php
+++ bd_games/games/save_new.php
@@ -2,17 +2,17 @@
  // Подключение к базе данных:
 $connect_error = 'Нет такой таблицы';
 $con = mysqli_connect('localhost', 'root');
-mysqli_select_db($con,'users') or die($connect_error);
+mysqli_select_db($con,'games') or die($connect_error);
  // Строка запроса на добавление записи в таблицу:
- $sql_add = "INSERT INTO user SET user_name='" . $_GET['name']
-."', user_login='".$_GET['login']."', user_password='"
-.$_GET['password']."', user_e_mail='".$_GET['e_mail'].
-"', user_info='".$_GET['info']. "'";
+ $sql_add = "INSERT INTO game SET game_name='" . $_GET['name']
+."', game_genre='".$_GET['genre']."', game_developer='"
+.$_GET['developer']."', game_publisher='".$_GET['publisher'].
+"', game_sale='".$_GET['sale']. "'";
  mysqli_query($con, $sql_add); // Выполнение запроса
  if (mysqli_affected_rows($con)>0) // если нет ошибок при выполнении запроса
- { print "<p>Спасибо, вы зарегистрированы в базе данных.";
- print "<p><a href=\"index.php\"> Вернуться к списку
-пользователей </a>"; }
- else { print "Ошибка сохранения. <a href=\"index.php\">
-Вернуться к списку книг </a>"; }
+ { print "<p>Спасибо, вы внесли информацию об игре.";
+ print "<p><a href=\"..\index.php\"> Вернуться к списку
+игр </a>"; }
+ else { print "Ошибка сохранения. <a href=\"..\index.php\">
+Вернуться к списку игр </a>"; }
 ?>
