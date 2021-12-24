9291797d9fc91bf325cec8de7729be63ea3ad65b
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 02:49:03 +0500

    Delete php-script for games

--- bd_games/games/delete.php
+++ bd_games/games/delete.php
@@ -1,7 +1,7 @@
 <?php
 $connect_error = 'Нет такой БД';
 $con = mysqli_connect('localhost', 'root','','games');
- $zapros="DELETE FROM game WHERE id_user='" . $_GET['id']."'";
+ $zapros="DELETE FROM game WHERE id_game='" . $_GET['id']."'";
  mysqli_query($con,$zapros);
  header("Location: ../index.php");
  exit;
