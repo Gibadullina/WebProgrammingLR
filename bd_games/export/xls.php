4af89b1296976cc7b0d382489425f6e1eda2a1cc
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 01:05:18 +0500

    export???

--- bd_games/export/xls.php
+++ bd_games/export/xls.php
@@ -6,32 +6,43 @@
 <?php
 
 require_once('Classes/PHPExcel.php');
-
+ if (isset($_GET["id"])) {
+	 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+ //$connect_error = 'Нет такой таблицы';
+ $con = new mysqli("localhost", "root", "", "games");
+//$con = mysqli_connect('localhost', 'root');
+ //mysqli_select_db($con,'games') or die($connect_error);
+ $zapr="SELECT id_game, game_name, game_genre, game_developer, game_publisher FROM game WHERE id_game='".$_GET['id']."'";
+ $rows=mysqli_query($con,$zapr);
+ while ($st = mysqli_fetch_array($rows,MYSQLI_BOTH)) {
+  echo $st[1];
+ }
+ }
 $dataArray = array(
  array(
  'str1raw1',
- 'str1raw2',
+ 'str',
  'str1raw3',
  'str1raw4',
  ),
  array(
- 'd',
+ $st[5],
  'str2raw2',
  'str2raw3',
  'str2raw4',
  )
 );
 
-// FILENAME
-$filename = "result.xlsx";
+// назва файла
+$filename = "Games.xlsx";
 
-// create php excel object
+// создание объекта php excel
 $doc = new PHPExcel();
 
-// set active sheet 
+// установить лист
 $doc->setActiveSheetIndex(0);
 
-// read data to active sheet
+// чтение данных в лист
 $doc->getActiveSheet()->fromArray($dataArray);
 
 //save our workbook as this file name
