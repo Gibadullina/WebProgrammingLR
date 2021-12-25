b43b6f3a63b4ac5edbc9709f77b291b15ec7c21f
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 11:25:36 +0500

    Edit.php done

--- bd_games/games/edit.php
+++ bd_games/games/edit.php
@@ -9,32 +9,32 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
  $con = new mysqli("localhost", "root", "", "games");
 //$con = mysqli_connect('localhost', 'root');
  //mysqli_select_db($con,'games') or die($connect_error);
- $zapr="SELECT game_name, game_genre,game_developer, game_publisher, game_sale game WHERE id_game='".$_GET['id']."'";
+ $zapr="SELECT game_name, game_genre,game_developer, game_publisher, game_sale FROM game WHERE id_game='".$_GET['id']."'";
  $rows=mysqli_query($con,$zapr);
  while ($st = mysqli_fetch_array($rows,MYSQLI_BOTH)) {
   $id=$_GET['id'];
  $name = $st['game_name'];
  $genre = $st['game_genre'];
- $password = $st['user_password'];
- $e_mail = $st['user_e_mail'];
- $info = $st['user_info'];
+ $developer = $st['game_developer'];
+ $publisher = $st['game_publisher'];
+ $sale = $st['game_sale'];
  }
 print "<form action='save_edit.php' metod='get'>";
-print "Имя: <input name='name' size='50' type='text'
+print "Название: <input name='name' size='50' type='text'
 value='".$name."'>";
-print "<br>Логин: <input name='login' size='20' type='text'
-value='".$login."'>";
-print "<br>Пароль: <input name='password' size='20' type='text'
-value='".$password."'>";
-print "<br>Е-mail: <input name='e_mail' size='30' type='text'
-value='".$e_mail."'>";
-print "<br>Информация: <textarea name='info' rows='4'
-cols='40'>".$info."</textarea>";
+print "<br>Жанр: <input name='genre' size='40' type='text'
+value='".$genre."'>";
+print "<br>Разработчик: <input name='developer' size='30' type='text'
+value='".$developer."'>";
+print "<br>Издатель: <input name='publisher' size='30' type='text'
+value='".$publisher."'>";
+print "<br>Объем продаж (млн): <input name='sale' size='11' type='number'
+value='".$sale."'>";
 print "<input type='hidden' name='id' value='".$id."'> <br>";
 print "<input type='submit' name='' value='Сохранить'>";
 print "</form>";
-print "<p><a href=\"index.php\"> Вернуться к списку
-пользователей </a>";
+print "<p><a href=\"..\index.php\"> Вернуться к списку
+игр </a>";
 ?>
 </body>
 </html>
