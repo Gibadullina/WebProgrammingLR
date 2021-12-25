4d7c540897448a5bb58eaacbc9d34d62f89878df
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-26 01:35:11 +0500

    Форма для создания ключа+ небольшие измен в индекс.пчп

--- bd_games/keys/new.php
+++ bd_games/keys/new.php
@@ -4,15 +4,12 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
  $con = new mysqli("localhost", "root", "", "games");
 //$con = mysqli_connect('localhost', 'root');
  //mysqli_select_db($con,'games') or die($connect_error);
- $zapr="SELECT  id_game, game_name,store_name FROM game, store";
+ $zapr="SELECT  id_game, game_name,store_name, id_store FROM game, store";
  $rows=mysqli_query($con,$zapr);
+ $i=0;
+ $k=0;
  while ($st = mysqli_fetch_array($rows,MYSQLI_BOTH)) {
-  $id=$_GET['game'];
- $name = $st['game_name'];
- $genre = $st['game_genre'];
- $developer = $st['game_developer'];
- $publisher = $st['game_publisher'];
- $sale = $st['game_sale'];
+  
  }
  ?>
 <p><html>
@@ -22,13 +19,14 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 <form action="save_new.php" metod="get">
  Дата приобретения: <input name="purchase" size="50" type="date">
 <br>Дата окончания: <input name="expiration" size="40" type="date">
-<P> Игра:  <SELECT NAME="z" SIZE="1">
- <OPTION VALUE="$id" SELECTED> <?php echo $name; ?>
- <OPTION VALUE="2"> госпожа
- <OPTION VALUE="3"> товарищ
- </SELECT>
+<P> Игра: <?php echo '<SELECT NAME="game" SIZE="1">';
+  for ($n=0;$n<=$i;$n++){
+ echo '<OPTION VALUE="'.$idg[$i].'" SELECTED>'. $name; 
+
+ echo '</SELECT>';
+ ?>
  <br>Стоимость: <input name="cost" size="11" type="number">
-<br>Издатель: <input name="publisher" size="30" type="text"
+<br>Ключ: <input name="dkey" size="30" type="text"
 <p><input name="add" type="submit" value="Добавить">
 <input name="b2" type="reset" value="Очистить"></p>
 </form>
@@ -36,3 +34,5 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 <a href="../index.php"> Вернуться к списку игр </a>
 </body>
 </html>
+<!--$con = mysqli_connect('localhost', 'f0597918_users','bnsniMin');
+mysqli_select_db($con,'f0597918_users') or die($connect_error);-->
--- bd_games/keys/save_new.php
+++ bd_games/keys/save_new.php
@@ -4,10 +4,10 @@ $connect_error = 'Нет такой таблицы';
 $con = mysqli_connect('localhost', 'root');
 mysqli_select_db($con,'games') or die($connect_error);
  // Строка запроса на добавление записи в таблицу:
- $sql_add = "INSERT INTO game SET game_name='" . $_GET['name']
-."', game_genre='".$_GET['genre']."', game_developer='"
-.$_GET['developer']."', game_publisher='".$_GET['publisher'].
-"', game_sale='".$_GET['sale']. "'";
+ $sql_add = "INSERT INTO digital_key SET purchase_date='" . $_GET['purchase']
+."', expiration_date='".$_GET['expiration']."',game='"
+.$_GET['game']."', store='".$_GET['store'].
+"', key_cost='".$_GET['cost']. "', digital_key='".$_GET['dkey']."'";
  mysqli_query($con, $sql_add); // Выполнение запроса
  if (mysqli_affected_rows($con)>0) // если нет ошибок при выполнении запроса
  { print "<p>Спасибо, вы внесли информацию о ключе.";
