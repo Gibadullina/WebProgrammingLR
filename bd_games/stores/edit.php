d5b9a20dcc29c3c4f5edf9d2ba51f4584325b235
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 21:18:06 +0500

    Форма для редакцтирпования сведений о store

--- bd_games/stores/edit.php
+++ bd_games/stores/edit.php
@@ -1,6 +1,6 @@
 <html>
 <head
-<title> Редактирование данных об играх </title>
+<title> Редактирование данных о цифровых магазинах </title>
 </head>
 <body>
 <?php
@@ -9,32 +9,22 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
  $con = new mysqli("localhost", "root", "", "games");
 //$con = mysqli_connect('localhost', 'root');
  //mysqli_select_db($con,'games') or die($connect_error);
- $zapr="SELECT game_name, game_genre,game_developer, game_publisher, game_sale FROM game WHERE id_game='".$_GET['id']."'";
+ $zapr="SELECT store_name, store_url FROM store WHERE id_store='".$_GET['id']."'";
  $rows=mysqli_query($con,$zapr);
  while ($st = mysqli_fetch_array($rows,MYSQLI_BOTH)) {
   $id=$_GET['id'];
- $name = $st['game_name'];
- $genre = $st['game_genre'];
- $developer = $st['game_developer'];
- $publisher = $st['game_publisher'];
- $sale = $st['game_sale'];
+ $name = $st['store_name'];
+ $url = $st['store_url'];
  }
 print "<form action='save_edit.php' metod='get'>";
 print "Название: <input name='name' size='50' type='text'
 value='".$name."'>";
-print "<br>Жанр: <input name='genre' size='40' type='text'
-value='".$genre."'>";
-print "<br>Разработчик: <input name='developer' size='30' type='text'
-value='".$developer."'>";
-print "<br>Издатель: <input name='publisher' size='30' type='text'
-value='".$publisher."'>";
-print "<br>Объем продаж (млн): <input name='sale' size='11' type='number'
-value='".$sale."'>";
+print "<br>URL: <input name='url' size='40' type='text'
+value='".$url."'>";
 print "<input type='hidden' name='id' value='".$id."'> <br>";
 print "<input type='submit' name='' value='Сохранить'>";
 print "</form>";
-print "<p><a href=\"..\index.php\"> Вернуться к списку
-игр </a>";
+print "<p><a href=\"..\index.php\"> Вернуться к спискам </a>";
 ?>
 </body>
 </html>
