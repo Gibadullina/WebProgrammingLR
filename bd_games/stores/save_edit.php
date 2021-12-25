2b4b34379f3414bfd833d2ba042f38023152421f
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 21:21:28 +0500

    Скрипт для сохранения измений в строках таблицы store

--- bd_games/stores/save_edit.php
+++ bd_games/stores/save_edit.php
@@ -5,20 +5,14 @@ $con = new mysqli("localhost", "root", "", "games");
  //$con=mysqli_connect("localhost","root","") or die ("Невозможно подключиться к серверу");
  //mysqli_select_db($con,"users") or die("Нет такой таблицы!");
  $g1=$_GET['name'];
- $g2=$_GET['genre'];
- $g3=$_GET['developer'];
- $g4=$_GET['publisher'];
- $g5=$_GET['sale'];
-  $zapros="UPDATE game SET game_name='".$g1.
-"', game_genre='".$g2."', game_developer='"
-.$g3."', game_publisher='".$g4.
-"', game_sale='".$g5."' WHERE id_game='".$_GET['id']."'";
+ $g2=$_GET['url'];
+  $zapros="UPDATE store SET store_name='".$g1.
+"', store_url='".$g2."' WHERE id_store='".$_GET['id']."'";
  mysqli_query($con,$zapros);
  //printf("Затронутые строки (UPDATE): %d\n", mysqli_affected_rows($con));
  if (mysqli_affected_rows($con)>0) {
- echo 'Все сохранено. <a href="../index.php"> Вернуться к списку
-игр </a>'; }
+ echo 'Все сохранено. <a href="../index.php"> Вернуться к спискам</a>'; }
  else { echo 'Ошибка сохранения. <a href="../index.php">
-Вернуться к списку игр</a> '; }
+Вернуться к спискам</a> '; }
 ?>
 </body> </html>
