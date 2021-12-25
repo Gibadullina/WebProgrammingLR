c1a1decebdd10a2564179bff5172e1f4c9505e9e
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-25 11:32:15 +0500

    save_edit done

--- bd_games/games/save_edit.php
+++ bd_games/games/save_edit.php
@@ -1,24 +1,24 @@
 <html> <body>
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$con = new mysqli("localhost", "root", "", "users");
+$con = new mysqli("localhost", "root", "", "games");
  //$con=mysqli_connect("localhost","root","") or die ("Невозможно подключиться к серверу");
  //mysqli_select_db($con,"users") or die("Нет такой таблицы!");
  $g1=$_GET['name'];
- $g2=$_GET['login'];
- $g3=$_GET['password'];
- $g4=$_GET['e_mail'];
- $g5=$_GET['info'];
-  $zapros="UPDATE user SET user_name='".$g1.
-"', user_login='".$g2."', user_password='"
-.$g3."', user_e_mail='".$g4.
-"', user_info='".$g5."' WHERE id_user='".$_GET['id']."'";
+ $g2=$_GET['genre'];
+ $g3=$_GET['developer'];
+ $g4=$_GET['publisher'];
+ $g5=$_GET['sale'];
+  $zapros="UPDATE game SET game_name='".$g1.
+"', game_genre='".$g2."', game_developer='"
+.$g3."', game_publisher='".$g4.
+"', game_sale='".$g5."' WHERE id_game='".$_GET['id']."'";
  mysqli_query($con,$zapros);
- printf("Затронутые строки (UPDATE): %d\n", mysqli_affected_rows($con));
+ //printf("Затронутые строки (UPDATE): %d\n", mysqli_affected_rows($con));
  if (mysqli_affected_rows($con)>0) {
- echo 'Все сохранено. <a href="index.php"> Вернуться к списку
-пользователей </a>'; }
- else { echo 'Ошибка сохранения. <a href="index.php">
-Вернуться к списку пользователей</a> '; }
+ echo 'Все сохранено. <a href="../index.php"> Вернуться к списку
+игр </a>'; }
+ else { echo 'Ошибка сохранения. <a href="../index.php">
+Вернуться к списку игр</a> '; }
 ?>
 </body> </html>
