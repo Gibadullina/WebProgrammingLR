dfce7c906c25d538e1d020bd6b4ffa54ae5c797f
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-26 23:56:18 +0500

    удалили ненужные файлы

--- bd_games_v2/delete_user.php
+++ bd_games_v2/delete_user.php
@@ -2,9 +2,7 @@
 include ("checkSession.php");
 $connect_error = 'Нет такой БД';
 $con = mysqli_connect('localhost', 'root','','games');
- $zapros="DELETE FROM store WHERE id_store='" . $_GET['id']."'";
- mysqli_query($con,$zapros);
- $zapros="DELETE FROM d_key WHERE store='" . $_GET['id']."'";
+ $zapros="DELETE FROM users WHERE id_users='" . $_GET['id']."'";
  mysqli_query($con,$zapros);
  header("Location: index.php");
  exit;
