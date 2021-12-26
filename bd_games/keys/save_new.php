5f4453c7160fc043f76876fbba96fa42f2cfc898
Gibadullina <gibadullina.elina@inbox.ru>
2021-12-26 12:32:24 +0500

    chsk

--- bd_games/keys/save_new.php
+++ bd_games/keys/save_new.php
@@ -4,7 +4,7 @@ $connect_error = 'Нет такой таблицы';
 $con = mysqli_connect('localhost', 'root');
 mysqli_select_db($con,'games') or die($connect_error);
  // Строка запроса на добавление записи в таблицу:
- $sql_add = "INSERT INTO digital_key SET purchase_date='" . $_GET['purchase']
+ $sql_add = "INSERT INTO d_key SET purchase_date='" . $_GET['purchase']
 ."', expiration_date='".$_GET['expiration']."',game='"
 .$_GET['game']."', store='".$_GET['store'].
 "', key_cost='".$_GET['cost']. "', digital_key='".$_GET['dkey']."'";
